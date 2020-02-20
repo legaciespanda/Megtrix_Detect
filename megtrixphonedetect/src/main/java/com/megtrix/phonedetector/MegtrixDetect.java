6537dbbd4d9a4c3fb98a18ee8f6350654ad3aefe
Ernest <youandinews@gmail.com>
2020-02-20 04:42:08 +0100

    Plugin Created

---
+++ megtrixphonedetect/src/main/java/com/megtrix/phonedetector/MegtrixDetect.java
@@ -0,0 +1,13 @@
+package com.megtrix.phonedetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MegtrixDetect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
