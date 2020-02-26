1ad72cc12bffba7644ba7da3a56dc4b3771aab9d
Ernest <youandinews@gmail.com>
2020-02-26 15:18:46 +0100

    Imported the library and implemented it

--- app/src/main/java/com/megtrix/phonedetect/MainActivity.java
+++ app/src/main/java/com/megtrix/phonedetect/MainActivity.java
@@ -3,12 +3,28 @@ package com.megtrix.phonedetect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.megtrix.phonedetector.MegtrixDetect;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.phoneTv);
+
+        MegtrixDetect detect = new MegtrixDetect();
+        detect.initiateSettings(this);
+
+
+        String insert = detect.getUserPhone();
+
+        textView.setText(insert);
+
+
     }
 }
